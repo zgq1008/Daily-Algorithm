45a13ed6f3355c721155bf2fcf845742c4acdb88
zgq1008 <3304354810@qq.com>
2026-02-23 20:57:31 +0800

    Time: 1 ms (57.82%), Space: 10.9 MB (8.38%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        std::string s = std::to_string(x);
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
