234c52e4b42fecb112cf32b4c179d487506df469
zgq1008 <3304354810@qq.com>
2026-03-01 13:54:36 +0800

    Time: 0 ms (100%), Space: 9.1 MB (1.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -7,7 +7,7 @@ char* longestCommonPrefix(char** strs, int strsSize) {
     for (i=0;strs[0][i]!='\0';i++)//遍历第一行字符串元素
     {
         char ch=strs[0][i];
-        for (int j=1;j<strsSize;j++)
+        for (int j=1;j<strsSize;j++)//遍历后续每一行字符串元素
         {
             if (strs[j][i]!=ch || strs[j][i]=='\0')
             {
@@ -17,6 +17,7 @@ char* longestCommonPrefix(char** strs, int strsSize) {
             }
         }
     }
+    //循环结束 第一行字符串就是最长前缀
     int len=strlen(strs[0]);
     strcpy(arr,strs[0]);
     arr[len]='\0';
