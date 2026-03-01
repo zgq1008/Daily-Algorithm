3b6ba037f2af1122ea9a5a5270b4aa46f088eeda
zgq1008 <3304354810@qq.com>
2026-03-01 16:08:04 +0800

    Time: 0 ms (100%), Space: 8.8 MB (1.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,47 +1,42 @@
-//判断所有字符串前n个元素是否相同 返回bool类型
-bool issame(char** strs,int len,int strsSize)
-{
-    char* arr=strs[0];
-    for (int i=1;i<strsSize;i++)
-    {
-        if (strncmp(arr,strs[i],len)!=0)
-        {
+// 判断所有字符串前n个元素是否相同 返回bool类型
+bool issame(char** strs, int len, int strsSize) {
+    char* arr = strs[0];
+    for (int i = 1; i < strsSize; i++) {
+        if (strncmp(arr, strs[i], len) != 0) {
             return false;
         }
     }
     return true;
 }
 char* longestCommonPrefix(char** strs, int strsSize) {
-   //二分查找
-   //最长字符串前缀肯定小于最短字符串的长度
-   if (strsSize==0)
+    // 二分查找
+    // 最长字符串前缀肯定小于最短字符串的长度
+    if (strsSize == 0)
         return "";
-    int minlen=strlen(strs[0]);
-    for (int i=1;i<strsSize;i++)
-    {
-        if (strlen(strs[i])<minlen)
-        {
-            minlen=strlen(strs[i]);
+        //寻找最短的字符串的长度
+    int minlen = strlen(strs[0]);
+    for (int i = 1; i < strsSize; i++) {
+        if (strlen(strs[i]) < minlen) {
+            minlen = strlen(strs[i]);
         }
     }
-    int low=0;
-    int high=minlen;
-    int mid=(low+high)/2;
-    while (low<=high)
-    {
-        if (issame(strs,mid,strsSize))
+    int low = 0;
+    int high = minlen;
+    int mid = (low + high) / 2;
+    while (low <= high) {
+        if (issame(strs, mid,
+                   strsSize)) // 从low到mid元素相同 则判断mid到high是否存在相同
         {
-            low=mid+1;
-            mid=(low+high)/2;
-        }
-        else
+            low = mid + 1;
+            mid = (low + high) / 2;
+        } else // 从low到mid元素不相同 则减小长度 继续判断
         {
-            high=mid-1;
-            mid=(low+high)/2;
+            high = mid - 1;
+            mid = (low + high) / 2;
         }
     }
-    char* arr=malloc((high+1)*sizeof(char));
-    strncpy(arr,strs[0],high);
-    arr[high]='\0';
+    char* arr = malloc((high + 1) * sizeof(char));
+    strncpy(arr, strs[0], high);
+    arr[high] = '\0';
     return arr;
 }
