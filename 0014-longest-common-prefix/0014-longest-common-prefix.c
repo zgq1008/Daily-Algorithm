0fac6f389f448a4681128d6fc2c14625bd8f4ab9
zgq1008 <3304354810@qq.com>
2026-03-01 16:04:06 +0800

    Time: 4 ms (1.49%), Space: 8.8 MB (1.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.c
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -1,26 +1,47 @@
-char* longestCommonPrefix(char** strs, int strsSize) {
-    //纵向扫描法
-    //二维数组
-    if (strsSize==0)
-        return "";
-    char* arr=malloc(201*sizeof(char));
-    int i;
-    for (i=0;strs[0][i]!='\0';i++)//遍历第一行字符串元素
+//判断所有字符串前n个元素是否相同 返回bool类型
+bool issame(char** strs,int len,int strsSize)
+{
+    char* arr=strs[0];
+    for (int i=1;i<strsSize;i++)
     {
-        char ch=strs[0][i];
-        for (int j=1;j<strsSize;j++)//遍历后续每一行字符串元素
+        if (strncmp(arr,strs[i],len)!=0)
         {
-            if (strs[j][i]!=ch || strs[j][i]=='\0')
-            {
-                strncpy(arr,strs[0],i);
-                arr[i]='\0';
-                return arr;
-            }
+            return false;
         }
     }
-    //循环结束 第一行字符串就是最长前缀
-    int len=strlen(strs[0]);
-    strcpy(arr,strs[0]);
-    arr[len]='\0';
+    return true;
+}
+char* longestCommonPrefix(char** strs, int strsSize) {
+   //二分查找
+   //最长字符串前缀肯定小于最短字符串的长度
+   if (strsSize==0)
+        return "";
+    int minlen=strlen(strs[0]);
+    for (int i=1;i<strsSize;i++)
+    {
+        if (strlen(strs[i])<minlen)
+        {
+            minlen=strlen(strs[i]);
+        }
+    }
+    int low=0;
+    int high=minlen;
+    int mid=(low+high)/2;
+    while (low<=high)
+    {
+        if (issame(strs,mid,strsSize))
+        {
+            low=mid+1;
+            mid=(low+high)/2;
+        }
+        else
+        {
+            high=mid-1;
+            mid=(low+high)/2;
+        }
+    }
+    char* arr=malloc((high+1)*sizeof(char));
+    strncpy(arr,strs[0],high);
+    arr[high]='\0';
     return arr;
 }
