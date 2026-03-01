c2767596263e363cd5348bbe1e9aedc5c48b2690
zgq1008 <3304354810@qq.com>
2026-03-01 13:53:24 +0800

    Time: 0 ms (100%), Space: 9 MB (1.08%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,24 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    //二维数组
+    if (strsSize==0)
+        return "";
+    char* arr=malloc(201*sizeof(char));
+    int i;
+    for (i=0;strs[0][i]!='\0';i++)//遍历第一行字符串元素
+    {
+        char ch=strs[0][i];
+        for (int j=1;j<strsSize;j++)
+        {
+            if (strs[j][i]!=ch || strs[j][i]=='\0')
+            {
+                strncpy(arr,strs[0],i);
+                arr[i]='\0';
+                return arr;
+            }
+        }
+    }
+    int len=strlen(strs[0]);
+    strcpy(arr,strs[0]);
+    arr[len]='\0';
+    return arr;
+}
