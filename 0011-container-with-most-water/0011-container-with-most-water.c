0d40aa5f139b8935a262706594a21a4037d7fa94
zgq1008 <3304354810@qq.com>
2026-03-15 20:27:43 +0800

    Time: 0 ms (100%), Space: 14.7 MB (17.99%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.c
+++ 0011-container-with-most-water/0011-container-with-most-water.c
@@ -7,6 +7,8 @@ int maxArea(int* height, int heightSize) {
         int minheight=height[r]>height[l]?height[l]:height[r];
         int area=minheight*(r-l);
         res=res>area?res:area;
+        // 核心优化：疯狂向内压缩，跳过所有不可能破纪录的矮板子
+        // 内部也要时刻保证 l < r，防止越界
         while(l<r&&height[l]<=minheight)
         {
             l++;
