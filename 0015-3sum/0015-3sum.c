a3c0b10ed69ac74d619ba6222363a91bdc9223ac
zgq1008 <3304354810@qq.com>
2026-03-17 15:00:15 +0800

    Time: 40 ms (82.47%), Space: 48.4 MB (7.22%) - LeetHub

--- 0015-3sum/0015-3sum.c
+++ 0015-3sum/0015-3sum.c
@@ -5,21 +5,23 @@
  * caller calls free().
  */
 // 1. 自己定义一个比较函数（升序）
-int cmp(const void *a, const void *b) {
-    return (*(int*)a - *(int*)b);
-}
-int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
-    qsort(nums, numsSize,sizeof(int),cmp);
-    *returnSize = 0;//存储一共有几行数据
-    int n = 30000;
+int cmp(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+int** threeSum(int* nums, int numsSize, int* returnSize,int** returnColumnSizes) {
+    // 进行排序 便于后续的指针的移动
+    qsort(nums, numsSize, sizeof(int), cmp);
+    *returnSize = 0; // 存储一共有几行数据
+    int n = 30000;   // 尽量大一些 避免溢出
     int** res = malloc(n * n * sizeof(int));
-    *returnColumnSizes = malloc(n * sizeof(int));//存储某一行数据多少列
+    *returnColumnSizes = malloc(n * sizeof(int)); // 存储某一行数据多少列
     int j = 0;
     int i, l, r;
     for (i = 0; i < numsSize - 2; i++) {
-        if (nums[i]>0) break;
+        if (nums[i] > 0)
+            break;
         if (i > 0 && nums[i] == nums[i - 1]) {
-            //跳过重复数字 进入下一循环
+            // 跳过重复数字 进入下一循环
+            // 防止作为第一个数字的 nums[i] 被重复使用
+            // 写成nums[i]==nums[i+1]会使-1 -1 2这种类似情况被忽略
             continue;
         }
         l = i + 1;
@@ -37,8 +39,11 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
                 res[*returnSize][2] = nums[r];
                 (*returnColumnSizes)[*returnSize] = 3;
                 (*returnSize)++;
-                while (l < r && nums[l] == nums[l + 1]) l++;
-                while (l < r && nums[r] == nums[r - 1]) r--;
+                // 跳过重复情况
+                while (l < r && nums[l] == nums[l + 1])
+                    l++;
+                while (l < r && nums[r] == nums[r - 1])
+                    r--;
                 l++;
                 r--;
             }
